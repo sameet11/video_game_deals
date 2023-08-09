fe09f7970577fe796fc22fa8e2d0bcb24eb868dd
sameet <sameetsabu1@gmail.com>
2023-08-09 13:56:18 +0530

    made changes in style of fecthed data

--- src/App.js
+++ src/App.js
@@ -5,27 +5,36 @@ function App() {
   const fetcher=async(url)=>{
     try{
      const response=await fetch(url);
-     if(!response.ok){
-      console.log('network error')
-     }
      const data= await response.json();
      console.log(data)
-     setword(null);
      return data;
     }
     catch(error){
       throw new Error(error);
     }
   }
-  const[word,setword]=useState(null);
-  const[input,setinput]=useState('');
-  const temp=useRef(null);
-  const search=()=>{
-    setword(input);
-    temp.current.value="";
+  const [word, setWord] = useState(null);
+  const [input, setinput] = useState('');
+  const [searchTriggered, setSearchTriggered] = useState(false);
+  const temp = useRef(null);
+
+  const search = () => {
+    setWord(input);
+    setSearchTriggered(true);
+    temp.current.value = '';
     setinput('');
-  }
-  const {data,error}=useSWR(word!==null?`https://www.cheapshark.com/api/1.0/games?title=${word}&limit=3`:null,fetcher);
+  };
+
+  const handlesearch = () => {
+    setSearchTriggered(false); // Prevent unnecessary refetch when input is focused
+  };
+
+  const { data } = useSWR(
+    searchTriggered
+      ? `https://www.cheapshark.com/api/1.0/games?title=${word}&limit=3`
+      : false,
+    fetcher
+  );
   return(
     <div className='App'>
       <div className='bg-cyan-400 py-24 flex flex-col items-center'>
@@ -33,18 +42,19 @@ function App() {
         <h1 className='font-bold text-4xl text-white'>Search For Game</h1>
         </div>
       <div className='mb-4'>
-        <input  onChange={(e)=>{setinput(e.target.value)}} type='text' placeholder='Enter Game to Search' className='bg-white w-full pl-2 rounded-md mb-4 h-10' ref={temp}/>
+        <input  onChange={(e)=>{setinput(e.target.value)}} onFocus={handlesearch}type='text' placeholder='Enter Game to Search' className='bg-white w-full pl-2 rounded-md mb-4 h-10' ref={temp}/>
         <button onClick={search}className='bg-white w-full rounded-md h-10'>Search Game</button>
       </div> 
       <div className='flex justify-evenly w-1/3 mx-auto'>
         {data&&data.map((item,ind)=>{
-          return (<div className='b-white' key={ind}>
-            <h1 className='text-2xl'>{item.external}</h1>
-            <img src={item.thumb} className='h-5 w-5'/>
+          return (<div className='bg-white p-3 border-2 rounded-md shadow-md w-1/4 mx-4' key={ind}>
+            <h1 className='text-lg font-bold text-center'>{item.external}</h1>
+            <img src={item.thumb}  alt="{item.external}" className='h-10 w-10 mx-auto'/>
           </div>);
         })}
       </div>
       </div>
+      <Deals/>
     </div>
   );
 }
